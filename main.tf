359e4170bb9529f00c62b49526ad02cc2d099f38
Lakshmana Sai Karumuri <k.lsai5421@gmail.com>
2026-03-30 10:50:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,27 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstatestorage"
-    container_name       = "tfstate"
-    key                  = "cicd/terraform.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "cicd" {
+  name     = "rg-cicd"
+  location = "East US"
+
+  tags = {
+    environment = "cicd"
+    managed_by  = "terraform"
   }
 }
